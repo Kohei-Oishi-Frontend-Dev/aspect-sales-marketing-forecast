fe0cbf6776b57a5238a264d8ad780e38e3e242e9
Payoshika <oishikohei@gmail.com>
2025-09-15 16:40:21 +0100

    import issue solved

--- app/login/LoginFom.tsx
+++ app/login/LoginFom.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useActionState, useEffect } from 'react'
+import React, { useState, useActionState, useEffect, JSX } from 'react'
 import { useRouter } from 'next/navigation';
 import { login } from './action'
 
