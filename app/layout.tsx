08e51877aff524ce5a8d59a834bc8a27e8f310dc
Payoshika <oishikohei@gmail.com>
2025-09-15 16:34:00 +0100

    session provider updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <SessionProvider>
+        <SessionProvider value={null}>
           <QueryProvider>{children}</QueryProvider>
         </SessionProvider>
       </body>
