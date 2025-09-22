80d869846428423e22d57a6d73b5f8e061f44804
Payoshika <oishikohei@gmail.com>
2025-09-22 15:10:52 +0100

    updated microsoft credential

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,15 +9,16 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   socialProviders: {
     microsoft: {
-      clientId: process.env.MICROSOFT_CLIENT_ID as string ,
+      clientId: process.env.MICROSOFT_CLIENT_ID as string,
       clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
       authorizeUrl: `https://login.microsoftonline.com/${process.env.MICROSOFT_TENANT_ID}/oauth2/v2.0/authorize`,
       tokenUrl: `https://login.microsoftonline.com/${process.env.MICROSOFT_TENANT_ID}/oauth2/v2.0/token`,
+      scope: "openid profile email",
     },
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-    }
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
   },
   plugins: [nextCookies()],
 });
