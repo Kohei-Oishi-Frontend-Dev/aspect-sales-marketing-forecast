f7c9f7892bf213e5a44f37e899141cf0ddd6581e
Payoshika <oishikohei@gmail.com>
2025-09-22 12:04:28 +0100

    updated microsoft sso login

--- lib/auth.ts
+++ lib/auth.ts
@@ -11,6 +11,8 @@ export const auth = betterAuth({
     microsoft: {
       clientId: process.env.MICROSOFT_CLIENT_ID as string ,
       clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
+      authorizeUrl: `https://login.microsoftonline.com/${process.env.MICROSOFT_TENANT_ID}/oauth2/v2.0/authorize`,
+      tokenUrl: `https://login.microsoftonline.com/${process.env.MICROSOFT_TENANT_ID}/oauth2/v2.0/token`,
     },
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
